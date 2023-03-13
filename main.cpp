61f28ff8a8b90d284755a994af7a57a64f966f95
durk <derrickmartinez2005@gmail.com>
2023-03-13 00:20:17 -0500

    Implemented SDL
    bout time

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <SDL.h>
+#include <SDL_image.h>
+
+const int WIN_WIDTH = 800;
+const int WIN_HEIGHT = 600;
 
 
 struct Student {
@@ -28,13 +33,13 @@ bool assignNames(std::map<std::string, Student> &studentMap) {
 }
 
 
-int main() {
+void originalCode() {
     srand(time(nullptr));
 
     std::map<std::string, Student> studentMap;
     if (!assignNames(studentMap)) {
         std::cout << "Failed to open names file.\n";
-        return 3;
+        return;
     }
 
     std::vector<std::string> majorList = {"Computer Science", "Business", "Culinary", "Nursing", "Philosophy",
@@ -48,7 +53,7 @@ int main() {
     courseList[majorList[3]] = {"Anatomy", "Biology", "Physical Education", "Statistics"};
     courseList[majorList[4]] = {"Ancient Philosophy", "Ethics", "Metaphysics", "Epistemology"};
     courseList[majorList[5]] = {"Administration", "Ethics", "Eyewitness Testimony", "Constitutional Law"};
-    for (auto& student: studentMap) {
+    for (auto &student: studentMap) {
         int randomIndex = rand() % courseList.size();
         std::string chosenMajor = majorList[randomIndex];
         student.second.major = chosenMajor;
@@ -70,8 +75,49 @@ int main() {
             std::cout << "Name: " << searchStudent.name << "\nMajor: " << searchStudent.major << "\n";
         }
     }
+}
 
-    std::cout << "hi";
+
+SDL_Texture *LoadTexture(const std::string &file, SDL_Renderer *ren) {
+    SDL_Texture *texture = nullptr;
+    SDL_Surface *surface = IMG_Load(file.c_str());
+    if (surface == nullptr) {
+        std::cout << "LoadTexture Error: " << SDL_GetError() << std::endl;
+    } else {
+        texture = SDL_CreateTextureFromSurface(ren, surface);
+        if (texture == nullptr) {
+            std::cout << "CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+        }
+    }
+    SDL_FreeSurface(surface);
+    return texture;
+}
+
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+    SDL_Window *window = SDL_CreateWindow("My App", 100, 100, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Texture *image = LoadTexture("../resources/logic.png", renderer);
+
+    bool run = true;
+    SDL_Event ev;
+    while(run) {
+        while(SDL_PollEvent(&ev) != 0) {
+            if(ev.type == SDL_QUIT) {
+                run = false;
+            }
+        }
+        SDL_RenderClear(renderer) ;
+        SDL_RenderCopy(renderer, image, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyTexture(image);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
 
     return 0;
 }
